3feb9151eb4b94fc947882e6424dc36024aecad2
geoffroyvilain <vilaingeoffroy02@gmail.com>
2019-01-25 10:29:30 +0100

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,41 @@
 function getNumber() {
+  var firstNumber = document.getElementById('firstNumber').value;
+  var secondNumber = document.getElementById('secondNumber').value;
+  //isNaN pour vérifier s'il ne s'agit pas d'un nombre.
+  //!isNaN = false isNaN = true
+  if(isNaN(firstNumber) == false && !isNaN(secondNumber)){
+    if(secondNumber != 0){
+  // != Différent
+      var result = firstNumber % secondNumber;
+  // % modulo
+      alert(result);
+    }else{
+      alert('Mettre un chiffre différent à 0 svp.');
+    }
+  }
+  else{
+    alert('Mettre un chiffre à partir de 1 svp.');
+  }
+  }
+
+
+
+
+/*
+  // pas de regex car pas utile, isNaN est plus approprié pour vérifier si ce sont des chiffres
 var firstNumber = document.getElementById('firstNumber').value;
 var secondNumber = document.getElementById('secondNumber').value;
-var result = firstNumber%secondNumber;
-if (isNaN(firstNumber)==false && isNaN(secondNumber)==false)
-{
-  alert(Math.floor(result));
+// isNaN est une fonction
+if (isNaN(firstNumber) == false && isNaN(secondNumber) == false) {     //!=false et sans !=true
+//                != --> différent
+  if{(secondNumber != 0)
+    var result = firstNumber % secondNumber;
+    alert(result);
+  }else{
+    alert('Merci de renseigner un chiffre différent de 0');
+  }
 }else{
-  alert('Merci de renseigner les champs');
+    alert('Mettre un chiffre supérieur à 1');
 }
 }
+*/
